6843e961c45d7db68427cf67dfc8c9b164128375
alessiofiorito <alessio.fiorito@outlook.com>
2025-01-31 23:51:50 +0100

    Integration of a grid, with all the rules for the game

--- include/Grid.h
+++ include/Grid.h
@@ -1,14 +1,27 @@
-//
-// Created by Alessio Fiorito on 31.01.2025.
-//
-
 #ifndef GRID_H
 #define GRID_H
 
+#include <vector>
 
 
 class Grid {
+public:
+    //constructor
+    Grid(unsigned int width, unsigned int height, unsigned int cellSize) : m_gridWidth(width), m_gridHeight(height),
+    m_cellSize(cellSize), grid(height, std::vector<bool>(width, false)) {}
 
+    bool isValidPosition(int x, int y) const;
+    bool isCellAlive(int x, int y) const;
+    unsigned int countLivingNeighbors(int x, int y) const;
+    void switchCellState(int x, int y);
+    void updateGrid();
+    void setCellAlive(int x, int y);
+
+private:
+    std::vector<std::vector<bool>> grid;
+    unsigned int m_gridWidth;
+    unsigned int m_gridHeight;
+    unsigned int m_cellSize;
 };
 
 
--- src/Grid.cpp
+++ src/Grid.cpp
@@ -1,5 +1,50 @@
-//
-// Created by Alessio Fiorito on 31.01.2025.
-//
-
 #include "Grid.h"
+#include <vector>
+
+bool Grid::isValidPosition(int x, int y) const {
+    return (x >= 0 && x < m_gridWidth && y >= 0 && y < m_gridHeight);
+}
+
+bool Grid::isCellAlive(int x, int y) const {
+    return grid[y][x];
+}
+
+unsigned int Grid::countLivingNeighbors(int x, int y) const {
+    unsigned int livingNeighbors = 0;
+
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            if (i == 0 && j == 0) continue;
+
+            if (isValidPosition(x + i, y + j) && grid[y + j][x + i]) {
+                ++livingNeighbors;
+            }
+        }
+    }
+
+    return livingNeighbors;
+}
+
+void Grid::switchCellState(int x, int y) {
+    if (isValidPosition(x, y)) {
+        grid[y][x] = !grid[y][x];
+    }
+}
+
+void Grid::updateGrid() {
+    std::vector<std::vector<bool>> tempGrid = grid;
+
+    for (int y = 0; y < m_gridHeight; ++y) {
+        for (int x = 0; x < m_gridWidth; ++x) {
+            unsigned int livingNeighbors = countLivingNeighbors(x, y);
+
+            if (grid[y][x]) {
+                tempGrid[y][x] = (livingNeighbors == 2 || livingNeighbors == 3);
+            } else {
+                tempGrid[y][x] = (livingNeighbors == 3);
+            }
+        }
+    }
+
+    grid = tempGrid;
+}
